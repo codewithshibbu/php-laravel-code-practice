1c81cc63f2dfe15440e1475bbcea0bd0e34c775e
SHIVAM GUPTA <sivamg951941@gmail.com>
2026-03-31 01:07:30 +0530

    Added: reverse, count even in array

--- basics/day-1.php
+++ basics/day-1.php
@@ -17,7 +17,26 @@ for($i=1;$i<count($arr);$i++){
 }
 echo "Largest: " . $max;
 
-// Q1. Reverse an array without using built in function
+// ----------------Q1. Reverse an array without using built in function
 $arr= [1,2,2,3,4,4,5];
 $reversedArr = [];
+for($i=count($arr)-1;$i>=0;$i--){
+   $reversedArr[]=$arr[$i];
+}
+echo "Reversed Array: ";
+print_r($reversedArr);
 
+//-----------------Q2. Count Even Numbers
+$arr= [1,2,3,4,5,6,7,8,9,10];
+$evenCount=0;
+for($i=0;$i<count($arr);$i++){
+    if($arr[$i]%2==0){
+        $evenCount+=1;
+    }
+}
+echo "Even Count: " . $evenCount;
+
+// Q3.---------- Remove Duplicates (IMPORTANT)
+
+$arr= [1,2,2,3,4,4,5];
+$uniqueArr = [];
