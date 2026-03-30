31ed197928b523400ef1d9ca94fe1ba1e0cc7896
SHIVAM GUPTA <sivamg951941@gmail.com>
2026-03-31 00:46:06 +0530

    Added: larget of array

--- basics/day-1.php
+++ basics/day-1.php
@@ -5,3 +5,19 @@ $arr = [10,20,30,40,50];
 for($i=0; $i<count($arr);$i++){
     echo $arr[$i]."\n";
 }
+
+// Practice 3: Find Largest Number
+
+$arr = [5, 8, 2, 10, 3];
+$max=$arr[0];
+for($i=1;$i<count($arr);$i++){
+    if($arr[$i]>$max){
+        $max=$arr[$i];
+    }
+}
+echo "Largest: " . $max;
+
+// Q1. Reverse an array without using built in function
+$arr= [1,2,2,3,4,4,5];
+$reversedArr = [];
+
